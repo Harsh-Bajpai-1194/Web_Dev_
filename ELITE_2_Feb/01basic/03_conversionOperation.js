ab22907e714b0927273f1fe677571d92768fdce9
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-02-10 16:18:38 +0530

    Delete ELITE_2_Feb/01basic directory

--- ELITE_2_Feb/01basic/03_conversionOperation.js
+++
@@ -1,8 +0,0 @@
-let score=33
-let valueInNumber=Number(score)
-let isLoggedIn="Harsh"
-console.log(typeof score);
-console.log(typeof(score));
-let score1=true
-console.log(typeof(""));
-console.log(Boolean(""));
